5402c5c985f032444f2295d5439e183473e44bab
Josh Chase <jcjoshuachase@gmail.com>
2013-06-19 07:13:51 -0400

    Improved OVPN code

--- ovpn/ovpn.go
+++ ovpn/ovpn.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"os/exec"
+	"os"
 )
 
 type OVPN struct {
@@ -20,37 +21,40 @@ type OVPN struct {
 	Cmd			*exec.Cmd
 }
 
-// Functions to spawn jobs that turn readers/writers to channels
-func spawnWriteChan(c io.WriteCloser) (o chan string) {
-	return
-}
-
-func spawnReadChan(c io.ReadCloser) (o chan string) {
-	return
-}
-
-func spawnReadWriteChan(c io.ReadWriteCloser) (o chan string) {
-	return
-}
+var ovpnOpts []string = []string{
+	"--mode","p2p",
+	"--proto","udp",
+	"--dev-type","tap",
+	"--tls-exit",
+	"--ping-exit","30",
+	"--ping","10"}
 
 func (v *OVPN) Connect() {
 	cmd := exec.Command(v.Path,
-		"--mode","p2p",
-		"--proto","udp",
-		"--dev-type","tap",
+	append(ovpnOpts,
 		"--remote",v.RemoteIP,
 		"--rport",v.RemotePort,
 		"--lport",v.LocalPort,
 		"--cert",v.CertFile,
 		"--key",v.KeyFile,
-		"--ifconfig",v.LocalTunIP,"255.255.255.252",
-		"--route",v.RemoteIP,"255.255.255.255",v.RemoteTunIP)
+		"--ifconfig",v.LocalTunIP,"255.255.255.252")...)
 
 	e := cmd.Run()
 
 	if e != nil {
 		log.Fatal(e.Error())
 	}
+	logFile,err := os.OpenFile(v.LogFile,os.O_APPEND,0666)
+	if err != nil {
+		log.Fatalf("Failed to open file for writing: %s",err)
+	}
+	errFile,err := os.OpenFile(v.ErrFile,os.O_APPEND,0666)
+	if err != nil {
+		log.Fatalf("Failed to open file for writing: %s",err)
+	}
+
+	go io.Copy(cmd.Stdout,logFile)
+	go io.Copy(cmd.Stderr,errFile)
 
 	v.Cmd = cmd
 	return
